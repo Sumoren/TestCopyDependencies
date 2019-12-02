9086690600fc7ccf8414e7b12ee9218ebc256bb8
Maxime Bourque-Fortin <maxime.bourque-fortin@ubisoft.com>
2019-12-02 08:40:33 -0500

    Add project files.

---
+++ TestCopyDependencies/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCopyDependencies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
